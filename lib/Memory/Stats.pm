500b1137a2ad660e4546c5d1f627534120f08cd5
Celogeek <me@celogeek.com>
2014-01-03 12:50:39 +0100

    init memory usage

--- lib/Memory/Stats.pm
+++ lib/Memory/Stats.pm
@@ -5,6 +5,62 @@ package Memory::Stats;
 use strict;
 use warnings;
 # VERSION
+use Proc::ProcessTable;
+use Carp qw/croak/;
 use Moo;
 
+my $pt = Proc::ProcessTable->new;
+my $current_memory_usage;
+my $delta_memory_usage;
+
+sub _get_current_memory_usage {
+	my %info = map { $_->pid => $_ } @{$pt->table};
+	return $info{$$}->rss;
+}
+
+=method start
+
+Init the recording
+
+=cut
+sub start {
+	$current_memory_usage = _get_current_memory_usage();
+	return;
+}
+
+=method stop
+
+Stop the recording
+
+=cut
+sub stop {
+	croak "Please call the method 'start' first !" if !defined $current_memory_usage;
+	$delta_memory_usage = _get_current_memory_usage() - $current_memory_usage;
+	$current_memory_usage = undef;
+	return;
+}
+
+=method get_memory_usage
+
+Return the last recording memory in KB
+
+=cut
+sub get_memory_usage {
+	croak "Please call the method 'start' then 'stop' first !" if !defined $delta_memory_usage;
+	return $delta_memory_usage;
+}
+
 1;
+
+__END__
+=head1 SYNOPSIS
+
+  use Memory::Stats;
+
+  my $stats = Memory::Stats->new;
+
+  $stats->start;
+  # do something
+  $stats->stop;
+  say "Memory consumed : ", $stats->get_memory_usage;
+
---
+++ t/mem-tests.t
@@ -0,0 +1,21 @@
+#!perl
+
+use Test::More;
+use Memory::Stats;
+
+my $stats = Memory::Stats;
+
+ok !eval {$stats->stop; 1}, 'start recording first';
+like $@, qr{\QPlease call the method 'start' first !\E}, 'error message ok';
+
+ok !eval {$stats->get_memory_usage; 1}, 'start and stop recording first';
+like $@, qr{\QPlease call the method 'start' then 'stop' first !\E}, 'error message ok';
+
+$stats->start;
+my %c = map { $_ => 1 } (1..100_000);
+ok !eval {$stats->get_memory_usage; 1}, 'start and stop recording first';
+like $@, qr{\QPlease call the method 'start' then 'stop' first !\E}, 'error message ok';
+$stats->stop;
+like $stats->get_memory_usage, qr{^\d+$}, 'memory usage ok';
+
+done_testing;
