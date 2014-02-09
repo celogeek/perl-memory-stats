f039c929222b17ece951fd2afc252f019da137ee
Celogeek <me@celogeek.com>
2014-02-09 18:11:20 +0100

    new api

--- lib/Memory/Stats.pm
+++ lib/Memory/Stats.pm
@@ -8,12 +8,10 @@ use warnings;
 use Proc::ProcessTable;
 use Carp qw/croak/;
 use Moo;
-use MooX::PrivateAttributes;
 
 my $pt = Proc::ProcessTable->new;
 
-private_has '_current_memory_usage' => (is => 'rw');
-private_has '_delta_memory_usage' => (is => 'rw');
+has '_memory_usage' => (is => 'rw', default => sub {[]});
 
 sub _get_current_memory_usage {
 	my %info = map { $_->pid => $_ } @{$pt->table};
@@ -26,8 +24,24 @@ Init the recording
 
 =cut
 sub start {
-  shift->_current_memory_usage(_get_current_memory_usage);
+  my $self = shift;
+  $self->_memory_usage([["start",_get_current_memory_usage()]]);
 	return;
+}
+
+=method checkpoint
+
+Mark a step in the recording,
+
+  $mu->checkpoint('title of the checkpoint');
+
+=cut
+sub checkpoint {
+  my $self = shift;
+  my $title = shift // 'checkpoint';
+  croak "Please start first !" if ! scalar @{$self->_memory_usage} || $self->_memory_usage->[-1][0] eq 'stop';
+  push @{$self->_memory_usage}, [$title, _get_current_memory_usage()];
+  return;
 }
 
 =method stop
@@ -37,21 +51,70 @@ Stop the recording
 =cut
 sub stop {
   my $self = shift;
-	croak "Please call the method 'start' first !" if !defined $self->_current_memory_usage;
-	$self->_delta_memory_usage(_get_current_memory_usage() - $self->_current_memory_usage);
-	$self->_current_memory_usage(undef);
+  croak "Please start first !" if ! scalar @{$self->_memory_usage} || $self->_memory_usage->[-1][0] eq 'stop';
+  push @{$self->_memory_usage}, ['stop', _get_current_memory_usage()];
 	return;
 }
 
-=method get_memory_usage
 
-Return the last recording memory in KB
+=method delta_usage
+
+Get the current delta memory usage since the last checkpoint
+
+  $mu->start;
+  ...
+  say $mu->delta_usage
 
 =cut
-sub get_memory_usage {
+sub delta_usage {
   my $self = shift;
-	croak "Please call the method 'start' then 'stop' first !" if !defined $self->_delta_memory_usage;
-	return $self->_delta_memory_usage;
+  my $last_memory_usage = $self->_memory_usage->[-1][1]
+    or return;
+  return (_get_current_memory_usage() - $last_memory_usage);
+}
+
+=method usage
+
+Get the full usage
+
+  $mu->start;
+  ...
+  $mu->stop;
+
+  say "Memory::Usage: ", $mu->usage;
+
+=cut
+sub usage {
+  my $self = shift;
+  croak "Please start and stop before !" if scalar @{$self->_memory_usage} < 2 || $self->_memory_usage->[-1][0] ne 'stop';
+  return $self->_memory_usage->[-1][1] - $self->_memory_usage->[0][1];
+
+}
+
+
+=method dump
+
+Dump the full stack
+
+=cut
+
+sub dump {
+  my $self = shift;
+  print "--- Memory Usage ---\n";
+  my $prev;
+  for my $row(@{$self->_memory_usage}) {
+    if ($prev) {
+      if ($row->[0] eq 'stop') {
+        printf("%s: %d - delta: %d - total: %d\n", @$row, $row->[1] - $prev, $row->[1] - $self->_memory_usage->[0][1]);
+      } else {
+        printf("%s: %d - delta: %d\n", @$row, $row->[1] - $prev);
+      }
+    } else {
+      printf("%s: %d\n", @$row);
+    }
+    $prev = $row->[1];
+  }
+  print "--- Memory Usage ---\n";
 }
 
 1;
@@ -66,11 +129,11 @@ __END__
   $stats->start;
   # do something
   $stats->stop;
-  say "Memory consumed : ", $stats->get_memory_usage;
+  say "Memory consumed: ", $stats->get_memory_usage;
 
 =head1 DESCRIPTION
 
 This module give you the memory usage (resident RSS), of a part of your process. It use L<Proc::ProcessTable> and should work on all platforms supported by this module.
 
-You can check this link to for explanation : L<http://blog.celogeek.com/201312/394/perl-universal-way-to-get-memory-usage-of-a-process/>
+You can check this link to for explanation: L<http://blog.celogeek.com/201312/394/perl-universal-way-to-get-memory-usage-of-a-process/>
 
